2ba899c4286b6b9a32538da94057fbc07ea17418
AmirAbbas-PC <a.abbasj@yahoo.com>
2025-01-28 21:22:35 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_clean_architecture_task
+
+This project is for hiring progress of AbanTether company written by AmirAbbas Jannatian
---
+++ android/app/src/main/kotlin/ir/amirabbas/flutter_clean_architecture_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package ir.amirabbas.flutter_clean_architecture_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
