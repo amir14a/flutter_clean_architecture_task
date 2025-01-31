201b16b4b4afc04d6565b09c29fc76176e050262
AmirAbbas-PC <a.abbasj@yahoo.com>
2025-01-31 07:14:12 +0330

    Add tests for cubit and fix problems to pass the tests

--- lib/feature_modules/user_details/presentation/cubit/user_details_page_state.dart
+++ lib/feature_modules/user_details/presentation/cubit/user_details_page_state.dart
@@ -1,35 +1,51 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter_clean_architecture_task/feature_modules/user_details/data/dto/user_dto.dart';
 
 abstract class UserDetailsPageState {}
 
 class UserDetailsInitialState extends UserDetailsPageState {}
 
-class UserDetailsLoading extends UserDetailsPageState {}
+class UserDetailsLoading extends UserDetailsPageState with EquatableMixin {
+  @override
+  List<Object?> get props => [];
+}
 
 class UserPhoneSubmitting extends UserDetailsPageState {}
 
-class UserDetailsLoaded extends UserDetailsPageState {
+class UserDetailsLoaded extends UserDetailsPageState with EquatableMixin {
   final UserDto userDto;
 
   UserDetailsLoaded(this.userDto);
+
+  @override
+  List<Object?> get props => [userDto];
 }
 
-class UserDetailsFailedToLoad extends UserDetailsPageState {
+class UserDetailsFailedToLoad extends UserDetailsPageState with EquatableMixin {
   final String? message;
 
   UserDetailsFailedToLoad({this.message});
+
+  @override
+  List<Object?> get props => [message];
 }
 
-class UserPhoneFailedToSubmit extends UserDetailsPageState {
+class UserPhoneFailedToSubmit extends UserDetailsPageState with EquatableMixin {
   final String? message;
   final String phone;
 
   UserPhoneFailedToSubmit({this.message, required this.phone});
+
+  @override
+  List<Object?> get props => [message, phone];
 }
 
-class UserPhoneSubmitted extends UserDetailsPageState {
+class UserPhoneSubmitted extends UserDetailsPageState with EquatableMixin {
   final String? message;
   final String phone;
 
   UserPhoneSubmitted({this.message, required this.phone});
+
+  @override
+  List<Object?> get props => [message, phone];
 }
